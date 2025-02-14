55a59044c143fad54f0b433aee48723cc577eea0
Vivi <viviana.rosas1999@gmail.com>
2025-02-14 14:58:00 -0800

    initial changes and setup of project

---
+++ HelloWPFApp/Constants/Ownership.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingTracking.Constants
+{
+    public enum Ownership
+    {
+        Library,
+        Pirated,
+        Bought,
+        Gifted
+    }
+}
---
+++ HelloWPFApp/Objects/Book.cs
@@ -0,0 +1,25 @@
+﻿using ReadingTracking.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingTracking.Objects
+{
+    public class Book
+    {
+        private string title;
+        private Status status;
+        private BookFormat bookformat;
+        private Ownership ownership;
+
+        public Book(string title, Status status, BookFormat bookformat, Ownership ownership)
+        {
+            this.title = title;
+            this.status = status;
+            this.bookformat = bookformat;
+            this.ownership = ownership;
+        }
+    }
+}
